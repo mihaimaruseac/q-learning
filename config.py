d26010a912043d63e1e1c70e35c4b663cef68904
Mihai Maruseac <mihai.maruseac@rosedu.org>
2011-04-09 09:30:46 +0300

    Removed many class variables from Config, changed them to privates

--- config.py
+++ config.py
@@ -37,120 +37,116 @@ class Config(object):
         """
         Builds the entire GUI for this dialog.
         """
-        self._topVBox = gtk.VBox()
-        self._build_IO_gui()
-        self._checkHBox = gtk.HBox()
-        self._build_action_gui()
-        self._build_learning_gui()
-        self._topVBox.pack_start(self._checkHBox, False, False, 5)
-        self._d.vbox.add(self._topVBox)
+        _topVBox = gtk.VBox()
+        self._build_IO_gui(_topVBox)
+        _checkHBox = gtk.HBox()
+        self._build_action_gui(_checkHBox)
+        self._build_learning_gui(_checkHBox)
+        _topVBox.pack_start(_checkHBox, False, False, 5)
+        self._d.vbox.add(_topVBox)
 
-    def _build_IO_gui(self):
+    def _build_IO_gui(self, _topVBox):
         """
-        Builds the GUI for the UI actions: what file to read and whether to
-        display plots while learning (in a new window).
+        Builds the GUI for the UI actions: what file to read and how many
+        steps to take on each learning iteration.
 
-        The plots comparing the two learning methods or any other comparisons
-        will be done with a different tool, using the plots obtained via the
-        above mentioned window, which should be saved to a file. See README
-        for more.
+        _topVBox    VBox holding the widgets built by this function
         """
-        self._fileHBox = gtk.HBox()
-        self._fileLabel = gtk.Label('Input filename:')
-        self._fileHBox.pack_start(self._fileLabel, False, False, 5)
+        _fileHBox = gtk.HBox()
+        _topVBox.pack_start(_fileHBox, False, False, 5)
+        _fileLabel = gtk.Label('Input filename:')
+        _fileHBox.pack_start(_fileLabel, False, False, 5)
         self._fileChoose = gtk.FileChooserButton("Select input filename")
-        self._fileHBox.pack_start(self._fileChoose, True, True, 5)
-
-        self._rLabel = gtk.Label('Max steps:')
-        self._fileHBox.pack_start(self._rLabel, False, False, 5)
+        _fileHBox.pack_start(self._fileChoose, True, True, 5)
+        _rLabel = gtk.Label('Max steps:')
+        _fileHBox.pack_start(_rLabel, False, False, 5)
         self._rCounter = gtk.SpinButton(gtk.Adjustment(step_incr=100))
         self._rCounter.set_numeric(True)
         self._rCounter.set_wrap(True)
         self._rCounter.set_range(100, 10000)
-        self._fileHBox.pack_start(self._rCounter, True, True, 5)
+        _fileHBox.pack_start(self._rCounter, True, True, 5)
 
-        self._topVBox.pack_start(self._fileHBox, False, False, 5)
-
-    def _build_action_gui(self):
+    def _build_action_gui(self, _checkHBox):
         """
         Builds the action selection GUI. Basically, it should allow selecting
         between choosing action using ε-greedy method or using softmax
         selection. In both cases, there is one parameter to select, tweaking
         the process (ε or τ).
-        """
-        self._frameActionSelection = gtk.Frame('Action Selection')
-        self._checkHBox.pack_start(self._frameActionSelection, True, True, 5)
 
-        self._asVBox = gtk.VBox()
-        self._frameActionSelection.add(self._asVBox)
+        _checkHBox    HBox holding the widgets built by this function
+        """
+        _frameActionSelection = gtk.Frame('Action Selection')
+        _checkHBox.pack_start(_frameActionSelection, True, True, 5)
+
+        _asVBox = gtk.VBox()
+        _frameActionSelection.add(_asVBox)
 
         self._greedyAction = gtk.RadioButton(None, 'Use ε-greedy selection')
         self._greedyAction.connect('clicked', self.__on_greedy)
-        self._asVBox.add(self._greedyAction)
+        _asVBox.add(self._greedyAction)
 
-        self._eHBox = gtk.HBox()
-        self._eLabel = gtk.Label('ε value:')
-        self._eHBox.pack_start(self._eLabel, False, False, 5)
+        _eHBox = gtk.HBox()
+        _eLabel = gtk.Label('ε value:')
+        _eHBox.pack_start(_eLabel, False, False, 5)
         self._eCounter = gtk.SpinButton(gtk.Adjustment(step_incr=.05), digits=2)
         self._eCounter.set_numeric(True)
         self._eCounter.set_wrap(True)
         self._eCounter.set_range(.1, .9)
-        self._eHBox.pack_start(self._eCounter, True, True, 5)
-        self._asVBox.add(self._eHBox)
+        _eHBox.pack_start(self._eCounter, True, True, 5)
+        _asVBox.add(_eHBox)
 
-        self._softmaxAction = gtk.RadioButton(self._greedyAction,
-                'Use softmax selection')
-        self._softmaxAction.connect('clicked', self.__on_softmax)
-        self._asVBox.add(self._softmaxAction)
+        _softmaxAction = gtk.RadioButton(self._greedyAction, 'Use softmax selection')
+        _softmaxAction.connect('clicked', self.__on_softmax)
+        _asVBox.add(_softmaxAction)
 
-        self._tHBox = gtk.HBox()
-        self._tLabel = gtk.Label('τ value:')
-        self._tHBox.pack_start(self._tLabel, False, False, 5)
+        _tHBox = gtk.HBox()
+        _tLabel = gtk.Label('τ value:')
+        _tHBox.pack_start(_tLabel, False, False, 5)
         self._tCounter = gtk.SpinButton(gtk.Adjustment(step_incr=.1), digits=1)
         self._tCounter.set_numeric(True)
         self._tCounter.set_wrap(True)
         self._tCounter.set_range(1, 10)
         self._tCounter.set_sensitive(False)
-        self._tHBox.pack_start(self._tCounter, True, True, 5)
-        self._asVBox.add(self._tHBox)
+        _tHBox.pack_start(self._tCounter, True, True, 5)
+        _asVBox.add(_tHBox)
 
-    def _build_learning_gui(self):
+    def _build_learning_gui(self, _checkHBox):
         """
         Builds the learning method selection GUI. Either Q-learning or SARSA
         is used. Also, it has to offer a way to select the two learning
         parameters (α and γ).
-        """
-        self._frameLearningMethod = gtk.Frame('Learning Method')
-        self._checkHBox.pack_start(self._frameLearningMethod, True, True, 5)
 
-        self._lmVBox = gtk.VBox()
-        self._frameLearningMethod.add(self._lmVBox)
+        _checkHBox    HBox holding the widgets built by this function
+        """
+        _frameLearningMethod = gtk.Frame('Learning Method')
+        _checkHBox.pack_start(_frameLearningMethod, True, True, 5)
+        _lmVBox = gtk.VBox()
+        _frameLearningMethod.add(_lmVBox)
 
         self._ql = gtk.RadioButton(None, 'Use Q-learning')
-        self._lmVBox.add(self._ql)
+        _lmVBox.add(self._ql)
+        _sarsa = gtk.RadioButton(self._ql, 'Use SARSA')
+        _lmVBox.add(_sarsa)
 
-        self._sarsa = gtk.RadioButton(self._ql, 'Use SARSA')
-        self._lmVBox.add(self._sarsa)
-
-        self._aHBox = gtk.HBox()
-        self._aLabel = gtk.Label('α value:')
-        self._aHBox.pack_start(self._aLabel, False, False, 5)
+        _aHBox = gtk.HBox()
+        _aLabel = gtk.Label('α value:')
+        _aHBox.pack_start(_aLabel, False, False, 5)
         self._aCounter = gtk.SpinButton(gtk.Adjustment(step_incr=.05), digits=2)
         self._aCounter.set_numeric(True)
         self._aCounter.set_wrap(True)
         self._aCounter.set_range(0, 1)
-        self._aHBox.pack_start(self._aCounter, True, True, 5)
-        self._lmVBox.add(self._aHBox)
+        _aHBox.pack_start(self._aCounter, True, True, 5)
+        _lmVBox.add(_aHBox)
 
-        self._gHBox = gtk.HBox()
-        self._gLabel = gtk.Label('γ value:')
-        self._gHBox.pack_start(self._gLabel, False, False, 5)
+        _gHBox = gtk.HBox()
+        _gLabel = gtk.Label('γ value:')
+        _gHBox.pack_start(_gLabel, False, False, 5)
         self._gCounter = gtk.SpinButton(gtk.Adjustment(step_incr=.05), digits=2)
         self._gCounter.set_numeric(True)
         self._gCounter.set_wrap(True)
         self._gCounter.set_range(0, 1)
-        self._gHBox.pack_start(self._gCounter, True, True, 5)
-        self._lmVBox.add(self._gHBox)
+        _gHBox.pack_start(self._gCounter, True, True, 5)
+        _lmVBox.add(_gHBox)
 
     def display(self):
         """
