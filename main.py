354172b7f29e7ac1b301f6de9bdd829e47e58980
Mihai Maruseac <mihai.maruseac@rosedu.org>
2011-04-10 22:15:57 +0300

    Started plot

--- main.py
+++ main.py
@@ -4,9 +4,21 @@
 # (c) Mihai Maruseac, 341C3 (2011), mihai.maruseac@rosedu.org
 #
 
+import sys
+
 import src.gui
+import src.cmp_plot
+
+def usage():
+    print './ql.py : simulates a robot'
+    print './ql.py cmp [FILES] : compares several runs'
 
 if __name__ == '__main__':
-    src.gui.main()
-
+    if len(sys.argv) >= 3 and sys.argv[1] == 'cmp':
+        if not src.cmp_plot.main(sys.argv[2:]):
+            usage()
+    elif len(sys.argv) == 1:
+       src.gui.main()
+    else:
+        usage()
 
